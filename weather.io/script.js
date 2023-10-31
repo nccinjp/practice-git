cbe528f6180f4d7b9b3b55853f26b66317d2e9d5
NC <nana790616@gmail.com>
2023-10-31 15:18:27 +0900

    first commit

---
+++ weather.io/script.js
@@ -0,0 +1,42 @@
+let weather = {
+    apiKey: "436bf96706a1081d7368932705de594e",
+    fetchWeather: function (city) {
+      fetch(
+        "https://api.openweathermap.org/data/2.5/weather?q=" +
+          city +
+          "&units=metric&appid=" +
+          this.apiKey
+      )
+        .then((response) => response.json())
+        .then((data) => this.displayWeather(data))
+    },
+    displayWeather: function (data) {
+      const { name } = data;
+      
+      const { icon } = data.weather[0];
+
+      const { description } = data.weather[0];
+      const { temp, humidity } = data.main;
+      const { speed } = data.wind;
+
+      document.querySelector(".city").innerText = "Weather in " + name;
+      
+      
+      document.querySelector(".icon").src=
+      "https://openweathermap.org/img/wn/" + icon + ".png";
+      
+      
+      document.querySelector(".description").innerText = description;
+      document.querySelector(".temp").innerText = temp + "°C";
+      document.querySelector(".humidity").innerText =
+        "Humidity: " + humidity + "%";
+      document.querySelector(".wind").innerText =
+        "Windspeed: " + speed + " km/h";
+      document.querySelector(".weather").classList.remove("loading");
+    
+    },
+    search: function () {
+      this.fetchWeather(document.querySelector(".search-bar").value);
+    },
+  };
+  weather.fetchWeather("osaka");
